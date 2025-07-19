3e0c2620a808330891eca48ad0a631f4adef6582
Antônio Carlos Durães da Silva <garotoseis@gmail.com>
2025-07-18 22:30:38 -0300

    feat: implement exposure adjustment functionality with comprehensive tests

--- src/adapters/filters/lighting-contrast/exposure.ts
+++ src/adapters/filters/lighting-contrast/exposure.ts
@@ -1,31 +1,30 @@
 import { ExposureOperation } from '@/core/domain/filters/interfaces/operations/lighting-contrast/exposure';
 
-export const exposure: ExposureOperation =
-    ({ value }) =>
-    (image) => {
-        const data = image.data;
+function applyExposureFilter(
+    imageData: ImageData,
+    exposure: number,
+): ImageData {
+    const newImageData = new ImageData(imageData.width, imageData.height);
+    newImageData.data.set(new Uint8ClampedArray(imageData.data));
+    const multiplier = Math.pow(2, exposure / 100);
 
-        // O valor de "value" é mapeado de -100 a 100
-        // Um valor de 0 significa nenhuma alteração, valores positivos aumentam a exposição (mais claros),
-        // valores negativos diminuem a exposição (mais escuros).
-        const exposureFactor = Math.pow(2, value / 100); // Ajuste exponencial baseado no valor de `value`
+    if (multiplier === 1) {
+        return newImageData;
+    }
 
-        // Passo 1: Ajustar a exposição para cada pixel
-        for (let i = 0; i < data.length; i += 4) {
-            let r = data[i]; // Red
-            let g = data[i + 1]; // Green
-            let b = data[i + 2]; // Blue
+    const data = newImageData.data;
 
-            // Ajustar os valores de R, G e B com base na exposição
-            r = Math.min(255, Math.max(0, r * exposureFactor));
-            g = Math.min(255, Math.max(0, g * exposureFactor));
-            b = Math.min(255, Math.max(0, b * exposureFactor));
+    for (let i = 0; i < data.length; i += 4) {
+        data[i] = Math.min(255, data[i] * multiplier); // Red
+        data[i + 1] = Math.min(255, data[i + 1] * multiplier); // Green
+        data[i + 2] = Math.min(255, data[i + 2] * multiplier); // Blue
+    }
 
-            // Atualizar os valores dos canais de cor
-            data[i] = r;
-            data[i + 1] = g;
-            data[i + 2] = b;
-        }
+    return newImageData;
+}
 
-        return image;
+export const exposure: ExposureOperation = ({ value }) => {
+    return (image) => {
+        return applyExposureFilter(image, value);
     };
+};
--- src/core/domain/filters/interfaces/operations/lighting-contrast/exposure.ts
+++ src/core/domain/filters/interfaces/operations/lighting-contrast/exposure.ts
@@ -4,7 +4,7 @@ export type ExposureOperationParams = {
     /**
      * The exposure value to apply to the image.
      * A value of 0 means no change, negative values decrease exposure, and positive values increase exposure.
-     * The value is in the range of -100 to 100.
+     * The value is in the range of -200 to 200.
      **/
     value: number;
 };
